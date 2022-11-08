70088fb52575a7af2d3b81c2ad63cd56fb01a4d5
DESKTOP-NENJELC\pc <jysj8212@naver.com>
2022-11-08 21:46:14 +0900

    221108_설문_설문항목에대한 답변내용을 담을 Vor객체추가

---
+++ ws/ForWorks/src/main/java/com/kh/forworks/qa/vo/QAAnswerVo.java
@@ -0,0 +1,17 @@
+package com.kh.forworks.qa.vo;
+
+import lombok.Data;
+
+@Data
+public class QAAnswerVo {
+	
+	private String qaawNo;			//답변번호
+	private String qacgNo;			//설문항목번호
+	private String empNo;			//사원번호
+	private String qaawContent;		//답변내용
+	
+	//사원이름
+	private String empName;
+	//부서명
+	private String deptName;
+}
