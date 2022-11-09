b136f97af805fa2115ec2a5912afba4498b40787
DESKTOP-3KDMSRL\ssora <ssora0420@hufs.ac.kr>
2022-11-09 21:51:58 +0900

    plan/ mapper작업

--- ws/ForWorks/src/main/java/com/kh/forworks/planPop/controller/PlanPopController.java
+++ ws/ForWorks/src/main/java/com/kh/forworks/planPop/controller/PlanPopController.java
@@ -1,10 +1,12 @@
 package com.kh.forworks.planPop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.forworks.planPop.service.PlanPopService;
 import com.kh.forworks.planPop.vo.PlanPopVo;
 
 
@@ -13,8 +15,15 @@ import com.kh.forworks.planPop.vo.PlanPopVo;
 //일정 캘린더 화면 보여주기
 @RequestMapping("plan")
 public class PlanPopController {
-	@GetMapping("planPop")
 	
+	private final PlanPopService ps;
+	
+	@Autowired
+	public PlanPopController(PlanPopService ps) {
+		this.ps = ps;
+	}
+		
+	@GetMapping("planPop")
 	public String calendar() {
 		return "plan/planPop";
 	}
@@ -22,5 +31,12 @@ public class PlanPopController {
 	@PostMapping("planPop")
 	public String planPop(PlanPopVo vo) {
 		
+		int result = ps.addPlan(vo);
+		
+		if(result == 1) {
+			return "redirect:/";
+		} else {
+			return "redirect/errorPage";
+		}		
 	}
 }	
--- ws/ForWorks/src/main/java/com/kh/forworks/planPop/service/PlanPopService.java
+++ ws/ForWorks/src/main/java/com/kh/forworks/planPop/service/PlanPopService.java
@@ -1,7 +1,15 @@
 package com.kh.forworks.planPop.service;
 
+import com.kh.forworks.planPop.vo.PlanPopVo;
+
 public interface PlanPopService {
 	
 	//일정 생성
-	int addPlan(planPopVo vo);
+	int addPlan(PlanPopVo vo);
+	
+	//일정 등록
+	
+	//일정 삭제
+	
+	
 }
